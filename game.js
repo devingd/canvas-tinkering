8076a0e4eb8dc5a05995c1047114581bcb1f7455
Devin Durtschi <devin.durtschi@gmail.com>
2017-11-15 00:46:30 -0700

    Computer opponent chases coin. First to 5 wins. Added restart button. More responsive now, too.

--- game.js
+++ game.js
@@ -2,21 +2,41 @@
 var mario;
 var coin;
 var badGuy;
+var score; 
 
-function startGame() {
+function loadGame () {
+    gamingArena.load();
+}
+
+function startGame () {
     gamingArena.start();
     mario = new square(10, 10, "red", 0, 0);
-    coin = new square(10, 10, "yellow", Math.floor((Math.random() * gamingArena.canvas.width) - 10), Math.floor((Math.random() * gamingArena.canvas.height) - 10));
     badGuy = new square(10, 10, "green", gamingArena.canvas.width - 10, gamingArena.canvas.height - 10);
+    coin = new square(10, 10, "gold", Math.floor((Math.random() * gamingArena.canvas.width) - 10), Math.floor((Math.random() * gamingArena.canvas.height) - 10));
+    score = new scoreBoard();
 }
 
 var gamingArena = {
     canvas: document.createElement("canvas"),
-    start: function () {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+    load: function () {
+        this.canvas.width = 300;
+        this.canvas.height = 300;
         this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);      
+
+        var button = document.createElement("button");
+        button.innerHTML = "Restart";
+        button.id = "restart-button";
+        button.onclick = startGame;
+        document.body.appendChild(button);
+        
+        window.addEventListener('resize', this.resizeCanvas, false);
+        window.addEventListener('orientationchange', this.resizeCanvas, false);
+        this.resizeCanvas();
+        
+        startGame();
+    },
+    start: function () {
         this.interval = setInterval(updateGamingArena, 20);
         window.addEventListener('keydown', function (e) {
             gamingArena.key = e.keyCode;
@@ -27,22 +47,42 @@ var gamingArena = {
     },
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop: function () {
+        clearInterval(this.interval);
+    },
+    resizeCanvas: function () {
+        gamingArena.canvas.width = window.innerWidth - 20;
+        gamingArena.canvas.height = window.innerWidth - 20;
+        if (mario) mario.updateEdges();
+        if (badGuy) badGuy.updateEdges();
+        if (coin) coin.updateEdges();
+    }
+}
+
+function scoreBoard () {
+    this.update = function(text, posX, posY) {
+        ctx = gamingArena.context;
+        ctx.font = "15pt Consolas";
+        ctx.fillStyle = "black";
+        ctx.fillText(text, posX, posY);
     }
 }
 
 function square(width, height, color, x, y) {
-    this.gamearena = gamingArena;
+    this.score = 0;
     this.width = width;
     this.height = height;
     this.speedX = 0;
     this.speedY = 0;
     this.x = x;
     this.y = y;
+    this.hasMoved = false;
 
     // define edges of canvas
     var top = 0;
-    var rightEdge = this.gamearena.canvas.width - this.width;
-    var bottom = this.gamearena.canvas.height - this.height;
+    var rightEdge = gamingArena.canvas.width - this.width;
+    var bottom = gamingArena.canvas.height - this.height;
     var leftEdge = 0;
 
     this.update = function () {
@@ -55,6 +95,25 @@ function square(width, height, color, x, y) {
         this.y += this.speedY;
         this.detectEdge();
     }
+
+    this.collectCoin = function (c) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = c.x;
+        var otherright = c.x + (c.width);
+        var othertop = c.y;
+        var otherbottom = c.y + (c.height);
+        if ((mybottom < othertop) ||
+            (mytop > otherbottom) ||
+            (myright < otherleft) ||
+            (myleft > otherright)) {
+            return false;
+        }
+        return true;
+    }
+ 
     this.detectEdge = function () {
         if (this.y < top) {
             this.y = top;
@@ -73,60 +132,91 @@ function square(width, height, color, x, y) {
             this.speedX = 0;
         }
     }
+
+    this.updateEdges = function () {
+        rightEdge = gamingArena.canvas.width - this.width;
+        bottom = gamingArena.canvas.height - this.height;
+    }
 }
 
 function updateGamingArena() {
     gamingArena.clear();
-    if (gamingArena.key && gamingArena.key == 37) {  // Left Arrow
-        if (mario.x > 0) {
-            mario.speedX = -1;
-            mario.speedY = 0;
+    if (mario.score == 5) {
+        score.update("YOU WIN!", gamingArena.canvas.width / 2 - 60, gamingArena.canvas.height / 2 - 20);
+        gamingArena.stop();
+    } else if (badGuy.score == 5) {
+        score.update("YOU LOSE...", gamingArena.canvas.width / 2 - 60, gamingArena.canvas.height / 2 - 20);
+        gamingArena.stop();
+    } else {
+        // Detect keyboard press
+        if (gamingArena.key && gamingArena.key == 37) {  // Left Arrow
+            mario.hasMoved = true;
+            if (mario.x > 0) {
+                mario.speedX = -3;
+                mario.speedY = 0;
+            }
         }
-    }
-    if (gamingArena.key && gamingArena.key == 39) {  // Right Arrow
-        if (mario.x < (gamingArena.canvas.width - mario.width)) {
-            mario.speedX = 1;
-            mario.speedY = 0;
+        if (gamingArena.key && gamingArena.key == 39) {  // Right Arrow
+            mario.hasMoved = true;
+            if (mario.x < (gamingArena.canvas.width - mario.width)) {
+                mario.speedX = 3;
+                mario.speedY = 0;
+            }
         }
-    }
-    if (gamingArena.key && gamingArena.key == 38) { // Up Arrow
-        if (mario.y > 0) {
-            mario.speedY = -1;
-            mario.speedX = 0;
+        if (gamingArena.key && gamingArena.key == 38) { // Up Arrow
+            mario.hasMoved = true;
+            if (mario.y > 0) {
+                mario.speedY = -3;
+                mario.speedX = 0;
+            }
         }
-    }
-    if (gamingArena.key && gamingArena.key == 40) { // Down Arrow
-        if (mario.y < (gamingArena.canvas.height - mario.height)) {
-            mario.speedY = 1;
-            mario.speedX = 0;
+        if (gamingArena.key && gamingArena.key == 40) { // Down Arrow
+            mario.hasMoved = true;
+            if (mario.y < (gamingArena.canvas.height - mario.height)) {
+                mario.speedY = 3;
+                mario.speedX = 0;
+            }
         }
-    }
 
-    mario.newPos();
-    mario.update();
-    coin.newPos();
-    coin.update();
-    if(mario.x > badGuy.x) {
-        badGuy.speedX = .5;
-        badGuy.speedY = 0;
+        // update score
+        if (mario.collectCoin(coin)) {
+            mario.score++;
+            coin = new square(10, 10, "gold", Math.floor((Math.random() * gamingArena.canvas.width) - 10), Math.floor((Math.random() * gamingArena.canvas.height) - 10));
+        } else if (badGuy.collectCoin(coin)) {
+            badGuy.score++;
+            coin = new square(10, 10, "gold", Math.floor((Math.random() * gamingArena.canvas.width) - 10), Math.floor((Math.random() * gamingArena.canvas.height) - 10));
+        }
+        score.update("Score: " + mario.score + " - " + badGuy.score, gamingArena.canvas.width / 2 - 60, 20);
+
+        // update player position
+        mario.newPos();
+        mario.update();
+        coin.newPos();
+        coin.update();
+        if(mario.hasMoved) {
+            if(coin.x > badGuy.x) {
+                badGuy.speedX = 1.5;
+                badGuy.speedY = 0;
+            }
+            else if (coin.x == badGuy.x) {
+                badGuy.speedX = 0;
+                badGuy.speedY = 1.5;
+            }
+            else {
+                badGuy.speedX = -1.5;
+                badGuy.speedY = 0;
+            }
+            if(coin.y > badGuy.y) {
+                badGuy.speedY = 1.5;
+            }
+            else if (coin.y == badGuy.y) {
+                badGuy.speedY = 0;
+            }
+            else {
+                badGuy.speedY = -1.5;
+            }
+        }
+        badGuy.newPos();
+        badGuy.update();
     }
-    else if (mario.x == badGuy.x) {
-        badGuy.speedX = 0;
-        badGuy.speedY = .5;
-    }
-    else {
-        badGuy.speedX = -.5;
-        badGuy.speedY = 0;
-    }
-    if(mario.y > badGuy.y) {
-        badGuy.speedY = .5;
-    }
-    else if (mario.y == badGuy.y) {
-        badGuy.speedY = 0;
-    }
-    else {
-        badGuy.speedY = -.5;
-    }
-    badGuy.newPos();
-    badGuy.update();
 }
